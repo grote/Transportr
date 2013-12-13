73a0e6b4eba9c4d728bbb9eb3d4f38b617703914
Torsten Grote <t@grobox.de>
2013-12-13 21:25:46 +0100

    use Android ActionMode for selecting trips

--- src/de/grobox/liberario/TripsActivity.java
+++ src/de/grobox/liberario/TripsActivity.java
@@ -40,11 +40,14 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TableLayout;
@@ -54,6 +57,8 @@ import android.widget.TextView;
 public class TripsActivity extends Activity {
 	private QueryTripsResult trips;
 	private Menu mMenu;
+	private ActionMode mActionMode;
+	private ViewGroup mSelectedTrip;
 	private Location from;
 	private Location to;
 
@@ -229,7 +234,43 @@ public class TripsActivity extends Activity {
 				trip_layout.setOnClickListener(new View.OnClickListener() {
 					@Override
 					public void onClick(View v) {
-						showTripDetails(v.getTag());
+						// take care of cases for ActionMode is already activated
+						if(mActionMode != null) {
+							if(v.isSelected()) {
+								// disable action mode for current item
+								mActionMode.finish();
+
+								// exit here to not start new ActionMode
+								return;
+							} else {
+								// deselect previously selected trip
+								mSelectedTrip.setSelected(false);
+
+								// disable action mode for current item
+								mActionMode.finish();
+							}
+						}
+
+						// select clicked trip
+						mSelectedTrip = (ViewGroup) v;
+						mSelectedTrip.setSelected(true);
+
+						// active new ActionMode for clicked trip
+						mActionMode = startActionMode(mTripActionMode);
+					}
+
+				});
+
+				// show more button for trip details
+				ImageView showMoreView = (ImageView) trip_layout.findViewById(R.id.showMoreView);
+				showMoreView.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						ViewGroup parent = ((ViewGroup) view.getParent());
+						View v = (View) parent.getParent();
+						if(v != null) {
+							showTripDetails(v.getTag());
+						}
 					}
 
 				});
@@ -335,5 +376,44 @@ public class TripsActivity extends Activity {
 		}
 	}
 
+	private ActionMode.Callback mTripActionMode = new ActionMode.Callback() {
+		// Called when the action mode is created; startActionMode() was called
+		@Override
+		public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+			// Inflate a menu resource providing context menu items
+			MenuInflater inflater = mode.getMenuInflater();
+			inflater.inflate(R.menu.trip_actions, menu);
+			return true;
+		}
 
+		// Called each time the action mode is shown. Always called after onCreateActionMode,
+		// but may be called multiple times if the mode is invalidated.
+		@Override
+		public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+			return false; // Return false if nothing is done
+		}
+
+		// Called when the user selects a contextual menu item
+		@Override
+		public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+			switch(item.getItemId()) {
+				case R.id.action_trip_details:
+					if(mSelectedTrip != null) {
+						showTripDetails(mSelectedTrip.getTag());
+					}
+					// Action picked, so close the CAB
+					mode.finish();
+					return true;
+				default:
+					return false;
+			}
+		}
+
+		// Called when the user exits the action mode
+		@Override
+		public void onDestroyActionMode(ActionMode mode) {
+			mSelectedTrip.setSelected(false);
+			mActionMode = null;
+		}
+	};
 }
