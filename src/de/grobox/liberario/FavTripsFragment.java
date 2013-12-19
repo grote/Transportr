4b7c284d3697c8b125f28d521f3c71d6389bc03a
Torsten Grote <t@grobox.de>
2013-12-19 01:02:21 +0100

    improved list of favorite trips

--- src/de/grobox/liberario/FavTripsFragment.java
+++ src/de/grobox/liberario/FavTripsFragment.java
@@ -23,16 +23,24 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class FavTripsFragment extends LiberarioListFragment {
 	private FavTripArrayAdapter adapter;
+	private ActionMode mActionMode = null;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -46,29 +54,7 @@ public class FavTripsFragment extends LiberarioListFragment {
 	public void onActivityCreated(Bundle savedState) {
 		super.onActivityCreated(savedState);
 
-		// remove trip on long click
-		getListView().setOnItemLongClickListener(new OnItemLongClickListener() {
-			@Override
-			public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-				final FavTrip trip = (FavTrip) parent.getItemAtPosition(position);
-
-				new AlertDialog.Builder(getActivity())
-				.setMessage(getActivity().getResources().getString(R.string.clear_fav_trips))
-				.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int id) {
-						FavFile.unfavTrip(getActivity(), trip);
-						adapter.remove(trip);
-					}
-				})
-				.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int id) {
-						dialog.cancel();
-					}
-				})
-				.show();
-				return true;
-			}
-		});
+		getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 	}
 
 	@Override
@@ -80,18 +66,29 @@ public class FavTripsFragment extends LiberarioListFragment {
 		adapter.addAll(FavFile.getFavTripList(getActivity()));
 	}
 
-	@Override
-	public void onListItemClick(ListView l, View v, int position, long id) {
-		FavTrip trip = (FavTrip) l.getItemAtPosition(position);
+	private void checkTrip(View v, int position) {
+		getListView().setItemChecked(position, true);
 
-		AsyncQueryTripsTask query_trips = new AsyncQueryTripsTask(v.getContext());
-		query_trips.setFrom(trip.getFrom());
-		query_trips.setTo(trip.getTo());
+		CheckBox checkBox = (CheckBox) v.findViewById(R.id.checkBox);
+		checkBox.setChecked(true);
 
-		// remember trip
-		FavFile.useFavTrip(getActivity(), trip);
+		if(mActionMode == null) {
+			mActionMode = getActivity().startActionMode(mFavTripActionMode);
+		}
+	}
 
-		query_trips.execute();
+	private void uncheckTrip(View v, int position) {
+		getListView().setItemChecked(position, false);
+
+		CheckBox checkBox = (CheckBox) v.findViewById(R.id.checkBox);
+		checkBox.setChecked(false);
+
+		if (mActionMode == null) {
+			mActionMode = getActivity().startActionMode(mFavTripActionMode);
+		}
+		else if (mActionMode != null && getListView().getCheckedItemCount() == 0) {
+			mActionMode.finish();
+		}
 	}
 
 	private class FavTripArrayAdapter extends ArrayAdapter<FavTrip> {
@@ -100,10 +97,143 @@ public class FavTripsFragment extends LiberarioListFragment {
 		}
 
 		@Override
+		public View getView(final int position, View v, ViewGroup parent) {
+			if(v == null) {
+				LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				v = inflater.inflate(R.layout.fav_trip_list_item, parent, false);
+			}
+
+			// handle click on row
+			v.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					FavTrip trip = (FavTrip) getListView().getItemAtPosition(position);
+
+					AsyncQueryTripsTask query_trips = new AsyncQueryTripsTask(v.getContext());
+					query_trips.setFrom(trip.getFrom());
+					query_trips.setTo(trip.getTo());
+
+					// remember trip
+					FavFile.useFavTrip(getActivity(), trip);
+
+					query_trips.execute();
+				}
+			});
+
+			// select trip on long click
+			v.setOnLongClickListener(new OnLongClickListener() {
+				@Override
+				public boolean onLongClick(View v) {
+					if(getListView().getCheckedItemPositions().get(position)) {
+						uncheckTrip(v, position);
+					} else {
+						checkTrip(v, position);
+					}
+					return true;
+				}
+			});
+
+			FavTrip trip = (FavTrip) this.getItem(position);
+
+			TextView favFromView = (TextView) v.findViewById(R.id.favFromView);
+			favFromView.setText(trip.getFrom().uniqueShortName());
+			TextView favToView = (TextView) v.findViewById(R.id.favToView);
+			favToView.setText(trip.getTo().uniqueShortName());
+			TextView favCountView = (TextView) v.findViewById(R.id.favCountView);
+			favCountView.setText(String.valueOf(trip.getCount()));
+
+			// handle click on check box
+			CheckBox checkBox = (CheckBox) v.findViewById(R.id.checkBox);
+			checkBox.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					View parent = (View) v.getParent();
+					if(getListView().getCheckedItemPositions().get(position)) {
+						uncheckTrip(parent, position);
+					} else {
+						checkTrip(parent, position);
+					}
+				}
+			});
+
+			return v;
+		}
+
+		@Override
 		public boolean hasStableIds() {
 			return true;
 		}
 	}
 
+	private ActionMode.Callback mFavTripActionMode = new ActionMode.Callback() {
+		// Called when the action mode is created; startActionMode() was called
+		@Override
+		public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+			// Inflate a menu resource providing context menu items
+			MenuInflater inflater = mode.getMenuInflater();
+			inflater.inflate(R.menu.fav_trip_actions, menu);
+			return true;
+		}
+
+		// Called each time the action mode is shown. Always called after onCreateActionMode,
+		// but may be called multiple times if the mode is invalidated.
+		@Override
+		public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+			return false; // Return false if nothing is done
+		}
+
+		// Called when the user selects a contextual menu item
+		@Override
+		public boolean onActionItemClicked(final ActionMode mode, MenuItem item) {
+			switch(item.getItemId()) {
+				case R.id.action_trip_delete:
+					new AlertDialog.Builder(getActivity())
+					.setMessage(getActivity().getResources().getString(R.string.clear_fav_trips, getListView().getCheckedItemCount()))
+					.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+							SparseBooleanArray tmp = getListView().getCheckedItemPositions();
+
+							// loop over selected items and delete associated trips
+							for(int i = tmp.size()-1; i >= 0; i--) {
+								if(tmp.valueAt(i)) {
+									int pos = tmp.keyAt(i);
+									FavTrip trip = adapter.getItem(pos);
+									FavFile.unfavTrip(getActivity(), trip);
+									adapter.remove(trip);
+								}
+							}
+							mode.finish();
+						}
+					})
+					.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+							dialog.cancel();
+						}
+					})
+					.show();
+					return true;
+				default:
+					return false;
+			}
+		}
+
+		// Called when the user exits the action mode
+		@Override
+		public void onDestroyActionMode(ActionMode mode) {
+			SparseBooleanArray tmp = getListView().getCheckedItemPositions();
+
+			// loop over checked items and deselect them
+			for(int i = tmp.size()-1; i >= 0; i--) {
+				if(tmp.valueAt(i)) {
+					int pos = tmp.keyAt(i);
+
+					if(getListView().getChildAt(pos) != null) {
+						uncheckTrip(getListView().getChildAt(pos), pos);
+					}
+				}
+			}
+			mActionMode = null;
+		}
+	};
 }
 
