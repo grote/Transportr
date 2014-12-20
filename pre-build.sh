b87821b6ed9db90387db71fe2a5fb377d018c0a5
Torsten Grote <t@grobox.de>
2014-12-20 21:32:30 +1300

    disable support for Paris due to too many instabilities

--- pre-build.sh
+++ pre-build.sh
@@ -8,7 +8,7 @@ mkdir -p src/de/schildbach/pte/live
 cp test/de/schildbach/pte/live/Secrets.java.template src/de/schildbach/pte/live/Secrets.java
 
 # add non-upstream providers
-sed -i "s#MET#MET, HSL#" src/de/schildbach/pte/NetworkId.java
+sed -i "s#SYDNEY, MET#SYDNEY, HSL, USNY, MET#" src/de/schildbach/pte/NetworkId.java
 
 mvn clean package -DskipTests
 
