1c4f583021b857fbb76a101c6dac728fe0899ebd
Torsten Grote <t@grobox.de>
2016-02-11 09:53:21 -0200

    Introduce gradle-witness for dependencies, build OSMBonusPack from jcenter
    and bump versions

--- pre-build.sh
+++ pre-build.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-# OSMBonusPack: don't fail on lintErrors
-echo "
-android { lintOptions { abortOnError false } }" >> sublibs/OSMBonusPack/OSMBonusPack/build.gradle
-
-# OSMBonusPack: don't use maven stuff (Travis fails)
-sed -i "/apply from: '..\/maven.gradle'/d" sublibs/OSMBonusPack/OSMBonusPack/build.gradle
-
-
 # PTE: add non-upstream providers
 cd sublibs/public-transport-enabler/enabler
 git checkout -- src/de/schildbach/pte/NetworkId.java
