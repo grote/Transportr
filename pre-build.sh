61c2e83ad734ddae1b8ceea0a771414fa3bef2cb
Patrick Kanzler <dev@pkanzler.de>
2017-01-19 21:57:22 +0100

    update PTE (fixes NPE in AbstractNavitiaProvider)
    * removed MET according to changes in PTE dee663ea54d818b2d58d22cf4d16c54746e4aa5b
    * adapted pre-build.sh to these changes
    * remove strings in english

--- pre-build.sh
+++ pre-build.sh
@@ -3,5 +3,5 @@
 # PTE: add non-upstream providers
 cd sublibs/public-transport-enabler/enabler
 git checkout -- src/de/schildbach/pte/NetworkId.java
-sed -i "s#SYDNEY, MET#SYDNEY, NZ, SPAIN, BR, BRFLORIPA, MET#" src/de/schildbach/pte/NetworkId.java
+sed -i "s#ONTARIO, QUEBEC#ONTARIO, NZ, SPAIN, BR, BRFLORIPA, QUEBEC#" src/de/schildbach/pte/NetworkId.java
 
