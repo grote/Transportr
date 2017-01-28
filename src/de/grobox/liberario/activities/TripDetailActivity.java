aec81e18c63764366fec1ccf14dfef461cce3589
Torsten Grote <t@grobox.de>
2017-01-28 14:57:33 -0200

    Disable trip detail return transition on Android 7.1
    Works around #285

--- src/de/grobox/liberario/activities/TripDetailActivity.java
+++ src/de/grobox/liberario/activities/TripDetailActivity.java
@@ -18,6 +18,7 @@
 package de.grobox.liberario.activities;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
@@ -62,6 +63,11 @@ public class TripDetailActivity extends TransportrActivity implements AsyncQuery
 		setContentView(R.layout.activity_trip_details);
 
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+
+		// work around #285
+		if(Build.VERSION.SDK_INT == Build.VERSION_CODES.N_MR1) {
+			getWindow().setSharedElementReturnTransition(null);
+		}
 
 		TransportNetwork network = Preferences.getTransportNetwork(this);
 		showLineName = network != null && network.hasGoodLineNames();
