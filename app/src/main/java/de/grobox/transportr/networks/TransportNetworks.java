7a357649618229e6c12ef4cd2e109bf26a062679
Torsten Grote <t@grobox.de>
2017-11-10 12:40:22 -0200

    Add missing and update existing copyright notices

--- app/src/main/java/de/grobox/transportr/networks/TransportNetworks.java
+++ app/src/main/java/de/grobox/transportr/networks/TransportNetworks.java
@@ -1,5 +1,7 @@
 /*
- *    Copyright (C) 2013 - 2016 Torsten Grote
+ *    Transportr
+ *
+ *    Copyright (c) 2013 - 2017 Torsten Grote
  *
  *    This program is Free Software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as
