d43d5fd28834c52be9b6f4a37956c0788f99ef76
Ialokim <ialokim@t-online.de>
2019-01-22 19:37:00 +0100

    Revert "Directions Form: Move overflow menu items into UI"
    This reverts commit 13a1a28fed1d8bdc499de099567b95f5e064855f.

--- app/src/main/java/de/grobox/transportr/trips/search/DirectionsViewModel.java
+++ app/src/main/java/de/grobox/transportr/trips/search/DirectionsViewModel.java
@@ -162,11 +162,6 @@ public class DirectionsViewModel extends SavedSearchesViewModel implements TimeD
 		return isDeparture;
 	}
 
-	void toggleDeparture() {
-		isDeparture.setValue(!(isDeparture.getValue() == null || isDeparture.getValue()));
-		search();
-	}
-
 	void setIsDeparture(boolean departure) {
 		isDeparture.setValue(departure);
 		search();
