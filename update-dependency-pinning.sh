b0e9d29f8d7a3b64daebdcc0fc7aef4fc3ca559f
Torsten Grote <t@grobox.de>
2019-04-30 14:43:50 -0300

    Hack to exclude jetified kotlin compiler that breaks gradle witness
    See walleth/walleth#354

--- update-dependency-pinning.sh
+++ update-dependency-pinning.sh
@@ -3,5 +3,5 @@
 WITNESS=app/witness.gradle
 
 echo "" > $WITNESS
-gradle -q calculateChecksums | grep -v "^\(Skipping\|Verifying\|        'transforms-1\)" > $WITNESS
+gradle -q calculateChecksums | grep -v "^\(Skipping\|Verifying\|        'transforms-1\)" | grep -v transforms-2 > $WITNESS
 
