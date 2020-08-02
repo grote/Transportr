0b21355c0f9cec9cf8b77fb0c7a85a194c7aa060
ialokim <ialokim@mailbox.org>
2020-08-02 19:08:07 +0200

    remove linux dependencies from witness.gradle

--- update-dependency-pinning.sh
+++ update-dependency-pinning.sh
@@ -3,5 +3,5 @@
 WITNESS=app/witness.gradle
 
 echo "" > $WITNESS
-./gradlew -q calculateChecksums | grep -Ev "^(Skipping|Verifying)" | grep -Ev "files-2.1:|:build-tools:core-lambda-stubs.jar:|:platforms:android.jar:" > $WITNESS
+./gradlew -q calculateChecksums | grep -Ev "^(Skipping|Verifying)" | grep -Ev "files-2.1:|:build-tools:core-lambda-stubs.jar:|:platforms:android.jar:|-linux.jar:" > $WITNESS
 
