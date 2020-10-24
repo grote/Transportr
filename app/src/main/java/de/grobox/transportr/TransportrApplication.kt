30d84eac9edd0141ca4a94c7b757f1423a20c362
ialokim <ialokim@mailbox.org>
2020-10-24 15:36:58 +0200

    convert TransportrApplication to Kotlin

---
+++ app/src/main/java/de/grobox/transportr/TransportrApplication.kt
@@ -0,0 +1,46 @@
+/*
+ *    Transportr
+ *
+ *    Copyright (c) 2013 - 2018 Torsten Grote
+ *
+ *    This program is Free Software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.grobox.transportr
+
+import android.app.Application
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.services.android.telemetry.MapboxTelemetry
+
+open class TransportrApplication : Application() {
+    lateinit var component: AppComponent
+        private set
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Mapbox.getInstance(
+            applicationContext,
+            "pk.eyJ1IjoidG92b2s3IiwiYSI6ImNpeTA1OG82YjAwN3YycXA5cWJ6NThmcWIifQ.QpURhF9y7XBMLmWhELsOnw"
+        )
+        MapboxTelemetry.getInstance().isTelemetryEnabled = false
+
+        component = createComponent()
+    }
+
+    protected open fun createComponent(): AppComponent {
+        return DaggerAppComponent.builder()
+            .appModule(AppModule(this))
+            .build()
+    }
+}
