b3efd237e30dc700f62aa82903bb25be601da530
Prat T <pt2121@users.noreply.github.com>
2023-06-22 08:10:39 -0700

    update update-dependency-pinning.sh

--- update-dependency-pinning.sh
+++ update-dependency-pinning.sh
@@ -3,5 +3,5 @@
 WITNESS=app/witness.gradle
 
 echo "" > $WITNESS
-./gradlew -q calculateChecksums | grep -Ev "^(Skipping|Verifying)" | grep -Ev "files-2.1:|caches:transforms-3:|:build-tools:core-lambda-stubs.jar:|:platforms:android.jar:|-linux.jar:" > $WITNESS
+./gradlew -q calculateChecksums | grep -Ev "^(Skipping|Verifying)" | grep -Ev "files-2.1:|caches:transforms-3:|:build-tools:core-lambda-stubs.jar:|:platforms:core-for-system-modules.jar:|-linux.jar:" > $WITNESS
 
