f15aaf8cee4d0ba5357877452ee0c0c09918b59d
Altonss <66519591+Altonss@users.noreply.github.com>
2024-11-10 12:19:43 +0100

    Remove all Navitia regions (#961)

--- app/src/main/java/de/grobox/transportr/networks/TransportNetworks.kt
+++ app/src/main/java/de/grobox/transportr/networks/TransportNetworks.kt
@@ -427,85 +427,6 @@ private val networks = arrayOf(
                     )
                 )
             ),
-            Country(
-                R.string.np_name_it, flag = "🇮🇹", networks = listOf(
-                    TransportNetwork(
-                        id = NetworkId.IT,
-                        name = R.string.np_name_it,
-                        description = R.string.np_desc_it,
-                        agencies = R.string.np_desc_it_networks,
-                        logo = R.drawable.network_it_logo,
-                        status = BETA,
-                        goodLineNames = true,
-                        factory = { ItalyProvider(NAVITIA) }
-                    )
-                )
-            ),
-            Country(
-                R.string.np_region_france, "🇫🇷", networks = listOf(
-                    TransportNetwork(
-                        id = NetworkId.PARIS,
-                        name = R.string.np_name_paris,
-                        description = R.string.np_desc_paris,
-                        agencies = R.string.np_desc_paris_networks,
-                        logo = R.drawable.network_paris_logo,
-                        status = BETA,
-                        factory = { ParisProvider(NAVITIA) }
-                    ),
-                    TransportNetwork(
-                        id = NetworkId.FRANCESOUTHWEST,
-                        name = R.string.np_name_frenchsouthwest,
-                        description = R.string.np_desc_frenchsouthwest,
-                        agencies = R.string.np_desc_frenchsouthwest_networks,
-                        logo = R.drawable.network_francesouthwest_logo,
-                        status = BETA,
-                        goodLineNames = true,
-                        factory = { FranceSouthWestProvider(NAVITIA) }
-                    ),
-                    TransportNetwork(
-                        id = NetworkId.FRANCENORTHEAST,
-                        name = R.string.np_name_francenortheast,
-                        description = R.string.np_desc_francenortheast,
-                        agencies = R.string.np_desc_francenortheast_networks,
-                        logo = R.drawable.network_francenortheast_logo,
-                        status = ALPHA,
-                        goodLineNames = true,
-                        factory = { FranceNorthEastProvider(NAVITIA) }
-                    ),
-                    TransportNetwork(
-                        id = NetworkId.FRANCENORTHWEST,
-                        name = R.string.np_name_francenorthwest,
-                        description = R.string.np_desc_francenorthwest,
-                        agencies = R.string.np_desc_francenorthwest_networks,
-                        logo = R.drawable.network_francenorthwest_logo,
-                        status = ALPHA,
-                        factory = { FranceNorthWestProvider(NAVITIA) }
-                    ),
-                    TransportNetwork(
-                        id = NetworkId.FRANCESOUTHEAST,
-                        name = R.string.np_name_frenchsoutheast,
-                        description = R.string.np_desc_frenchsoutheast,
-                        agencies = R.string.np_desc_frenchsoutheast_networks,
-                        logo = R.drawable.network_francesoutheast_logo,
-                        status = BETA,
-                        goodLineNames = true,
-                        factory = { FranceSouthEastProvider(NAVITIA) }
-                    )
-                )
-            ),
-            Country(
-                R.string.np_name_spain, flag = "🇪🇸", networks = listOf(
-                    TransportNetwork(
-                        id = NetworkId.SPAIN,
-                        name = R.string.np_name_spain,
-                        description = R.string.np_desc_spain,
-                        agencies = R.string.np_desc_spain_networks,
-                        logo = R.drawable.network_spain_logo,
-                        status = BETA,
-                        factory = { SpainProvider(NAVITIA) }
-                    )
-                )
-            ),
         )
     ),
     Continent(
@@ -560,15 +481,6 @@ private val networks = arrayOf(
             Country(
                 R.string.np_region_australia, flag = "🇦🇺", networks = listOf(
                     TransportNetwork(
-                        id = NetworkId.AUSTRALIA,
-                        name = R.string.np_name_australia,
-                        description = R.string.np_desc_australia,
-                        agencies = R.string.np_desc_australia_networks,
-                        logo = R.drawable.network_aus_logo,
-                        status = BETA,
-                        factory = { AustraliaProvider(NAVITIA) }
-                    ),
-                    TransportNetwork(
                         id = NetworkId.SYDNEY,
                         name = R.string.np_name_sydney,
                         description = R.string.np_desc_sydney,
@@ -581,7 +493,6 @@ private val networks = arrayOf(
     )
 )
 
-private const val NAVITIA = "87a37b95-913a-4cb4-ba52-eb0bc0b304ca"
 private const val VAO = "{\"aid\":\"hf7mcf9bv3nv8g5f\",\"pw\":\"87a6f8ZbnBih32\",\"type\":\"USER\",\"user\":\"mobile\"}"
 private val VRS: ByteArray = Base64.decode(
     "MIILOQIBAzCCCv8GCSqGSIb3DQEHAaCCCvAEggrsMIIK6DCCBZ8GCSqGSIb3DQEHBqCCBZAwggWMAgEAMIIFhQYJKoZIhvcNAQcBMBwGCiqGSIb3DQEMAQYwDgQITP1aoTF3ISwCAggAgIIFWBba5Nms7ssWBgCkVFboVo4EQSGNe6GvJLvlAIAPGBieMyQOeJJwDJgl422+dzIAr+wxYNTgXMBMf7ZwPpVLUyCECGcePHfbLKyAK5CqvP+zYdGYc8oHF5JcukK2wm0oCxt4sRvPKAimFjU1NWFVzX8HY8dTYia59nOF1dk7LmfA5wI8Jr2YURB71lycHLvm4KbBl23AZmEgaAGWPcHhzPFfslo8arlixKGJqc02Tq9gA0+ZY/nkvNtl7fEbVJkHXF7QP7D5O7N5T6D2THyad9rqVdS499VwQ16b5lBTgV5vWD5Ctf5riuewc4aUziGLnukBrHgWOHK8TfsAhtTOrUerAFLNVB2jF6nBKbgywBXKYOBDhKX3MdVmt3srkq0/Ta2+bxUHfwRt17EQKFzboiNuraALs2jXrbSHvuO+pV2yj0WP/sX8d6KXf3XMFejynv7Os7sD0mQTcllsN9bf2oGVUnSaHT97RAekYxaF7LX+q94rhXmhpFPH/ILQEt92lF+nk+XlmhlGT9SUhwUJ6AKysFRY7si/ofE+8V4ZFHDnyjoUNDhOUYC/Z4I7YpozuPECPKNReTbPdHXqlBIiEx243gutskl8duiGYEv7TzraAq0Nag6Xk8YcXoyMXGC8wrecU7Uts9Tm2OBErAqxvFWXL9eN/EsYV8SB745tmU+T4EqJDDZQZnRAerg7Ms4iSKSbPNj/OtwpIptv43NWAtyzEEc6NxwwQTIJZL0v9jwB0mUY7TgM4a+VwMTBHcBNZH5+x8dpwh1H8MYh91UaBOidbc2PJeLtT4pIxYlcyYGl9LJa68WgzBkc7uJmETNOfKfdJEazLvH/jIRsLBwzPj/pbJDPER82wC8l5mmbOyNa/vgjsSAvm2uYDsV1fo8xdik3q/SFRHseIf2vQtybDXrytafUb9D6/0puTycMo5IfXegHvuwIJVhYFcqoCDX8VkkebHHWdWelr7yPealzjksddiJ9a4mksc4js3g7if5cQwYkfiVNE2FQukkjJx1xhgRCsnTRv1K0n0t1g4D5CD4oYjTBiYzgF/t2CqH85wNAVKnJmKNyt0Weqcf6GQwu0oVC+9IqSAiy07KvEbLxjjqcBarQjGKPSLmJeQ0x9X+9KIaEKG3gdN5l8ptlfHhML2wZsn0cTCBU1otOdLcu4QmBGf6DSTSCXcH4GGvlWdxjxdQ7Docmdp3hQBh8wY7jRST+YWcp5zQWkOpClFjKIKx2s+0sG7XM+LNPr2zSJZTyLcPlqdc9aam9LL3nf3CUtUNVrDaiyfTYhgpBHkwc+4P8MIsaZy8gowfBhovsYvfE5aFzF3rfLf30r31/ju/jkcfnWW995X+AJb8pcQuC6R7xJ82lZyPRpyfs96eCmizjIcAcL6Wz+SQEsUE3zNuH/ctpqhD5gCKXhJTj6sXjdiGNkYqPyxKX3blw8fdh+nIe3kBdC9deaw4S+5QYNKPSmdmQAAaOxOyzLi+DKgR9bV6SzWUAO/kWCdRaCdCDy9WS+6CQ2AVsQOSYv1vBMWkZ0u5/EHqPsb6y1wtXvE0/s7T4KZi7taP/72dDclPgNHsWCW5HbSaeyx83efu3fpX7i8tsWmr+QeeRuLGJ5z0NOBKasIKhCe3XPWZGNzKNca0WJk7UWepYFfiPv57tFj6Y0zautFHFNRgP+iu0hX7nNNn0AVXjuFFiZ/fwhjFmXExSYG9xSzcR5aJha0GEJ+MQbIZD7/Ay8GRmPFrrN8x40svTfiWu71qpxqsfco+2sKhJtBxJoO/cnjRz5PrtCdnqi4dYHtvOAyjaaF/3hQvDyiEoiDuxTPIVyjCCBUEGCSqGSIb3DQEHAaCCBTIEggUuMIIFKjCCBSYGCyqGSIb3DQEMCgECoIIE7jCCBOowHAYKKoZIhvcNAQwBAzAOBAg71M5exZmMVQICCAAEggTIohxJ2uLoi9RYzxe7t0XOHkTBSI+/Rn3oQNecNuMe/YNpMMsRCQjSOJToWHGayBQJmwSkMd3NP4QnDfqWFIxHbgnfj3FLTIyfkDIObzpfHwLCOrYHQxK9Zr4t/0SfEy/34uH40ZEiPe7Mnn/iTTZy37ecZgLsvlr6wp5Gao3oBjhKZlxJM043Hy9Dk1vtRCRIFCFbdGXtcLnuVKASc+GVw6QJKoXLerImV0U5Pg6khh0huTALEULuvq5cEIlKBNqyZ37cfb3Cvf9mWSTferBcUymGyHtdh+mHtVPb3ZycprtFmKcGMR9bXK0FJ63fERmXRHBN1ZKVC0beWVgcGybDQKdx9Y26UQLtO3xdZK0Eb3Kn8jVJG3sEJi2u3CLS4wD533+jj+b1uuL8Uj/aZy2UvrbIez48JStZgBGg+IhLK5keW7KV1lHiOVwZuWERpxzbNx7jaZRWIUCwN+aMJts1d5aY+wYvlJ9uk2lQc8qpIDIHHXHvyUEnk7jxw88gQjNgo1lvUHewiQk6VBwXX7EII0kLxdNfEpBT9RAdqURqy8dpoQemoc2zwce0e14G+IElJ1ES1j2jMYkYuggjpfUJBc34QrQI2a7UQwloUMwkdoi9nwgnpeL5G3Jyvgfxxf+D9xSXh8auH5IsdO0/enDGo/Xo+ygQ3tgY3dGI02frzRF24i4hFp/FAdbLjytjgCF0KIEXbJylEweZX2g61jL/fJVowJIA3wXDSuIBq9YRdpEA2OhgCdpwcz69W9T5lVfuJBgKOKcFKSQgDm0sEEkcUV9WR4CWfC9lZ+haHvNcrJBsRkHg6KKsV8PwwbUs2WeXl3NvGnJ/kSQbqJOLfURPziY9w4phupuSTAqmQIc0D4MSZLEjDcXKjg3ifFi4NlGLy+iyzGBoC1YZk1OOlO3uhKxxSD8FG6ncRGHEr8OU+2Yj/qubqZMpckPLXPdWbZB24bQxPTKGeQjFGlgt95H3/aRK9FzmBLc1FOe4qnT9chzbewsAnuho+F7Rqe36hPCZHlIrND0RCOdTAw7buJg6yPIbpDA41SpvS1F/BdFuDepf4yd0NWt4N46zUHmpxavv+2zmDiAUG95ZQ7AmkAA39tc+XtQv3IhLK6Wa7joM61jtau34td3vi1RvN2fPY2jQqOvKA2/hTVw5SzWCI0Tl7le6+ol1/QeUJfpjBZl6Ai+ydgVycSXuyq+MXB/UUEWo8RmlX8R9+y2KtCGV0TQjfX/um1D77LzurRO430m2pggcxmdCiFyl4CRp+rXhw7W6nGwLqZfD2msKthh+tn2QxoNII1oGHHsF7fxE/E4wm54IGtqfLM5pV/5hrqgVfTetABMLFEbtIHrxEDms80SyvsP2/JgelFFrs90wZr9QkLVBBQtZpwmLu39u24HlGXhZflXX0fmlHT2vN1e/EH43Nl/iPgZPYTj6fGGJFdaKNm0QlLym2M0btN3MNMXHETUoLDOg17AomH3NRvSIARu92qa48rX+SeCdF0NJ3VmA2I3Fl4A47epkmMcCzF078UVPC2eQ9M2NtxIAsqQnfIFfxirTuSCdeVS06n8KbMi7PG4Luc7IUPr4W3SQ9mY8XjFgRjVl86QpExzE6P5WZ/RDrgaypcDED6BvMSUwIwYJKoZIhvcNAQkVMRYEFKkQDH5bs77hmpmQ899BQPMX5lIDMDEwITAJBgUrDgMCGgUABBSqWv+fwvAy3ohpbmU2hfBpJbEejAQIPczIVgsfvYECAggA",
